09b27546838ad3f91a8a50257c5578c491fece0c
Lanaanvar <118440051+Lanaanvar@users.noreply.github.com>
2023-07-24 23:20:46 +0530

    Add files via upload

---
+++ java/Add.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author lanaa
+ */
+public class Add {
+    public int x;
+    public int y;
+    
+    public void setx(int x)
+    {
+        this.x=x;
+    }
+    public void sety(int y)
+    {
+        this.y=y;
+    }
+    public int getx()
+    {
+        return x;
+    }
+    public int gety()
+    {
+        return y;
+    }
+    public int sum()
+    {
+        return x+y;
+    }
+    
+}
